2a219f7920e4109b2340620d6e69860a36aec0a4
rumeysayilma <75755454+rumeysayilma@users.noreply.github.com>
2020-12-10 22:58:28 +0300

    ikinci gün

--- data-generator.py
+++ data-generator.py
@@ -1,61 +1,92 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import savetxt
 
-#harflerin ve 4 bit hatası yapılmış versiyonlarının oluşturulması
+""" harflerin ve 4 bit hatası yapılmış versiyonlarının oluşturulması
+**Verimizdeki 0 değerlerini 0.1; 1 değerlerini ise 0.9'a alarak düzenledik. 
+Bu sayede 0.1 değerleri ağırlıklarla çarpıldığı zaman daha anlamlı sonuçlar bulunacak.
+t = np.where(t == 0, 0.1, 0.9)
+ """
 def pattern_designer():
-    t= np.ones((5, 10),dtype=float)
+    t= np.ones((5, 10))
     t [:, 5] = 0
     t[0, :] = 0
+    t = np.where(t == 0, 0.1, 0.9)
     t_degisik = np.copy(t)
-    t_degisik[0 ,0],t_degisik[1 ,0],t_degisik[3 ,4],t_degisik[3 ,5] = 1,0,0,1
-    l= np.ones((5, 10),dtype=float)
+    t_degisik[0 ,0],t_degisik[1 ,0],t_degisik[3 ,4],t_degisik[3 ,5] = 0.9,0.1,0.1,0.9
+    l= np.ones((5, 10))
     l[:, 0] = 0
     l[-1, :] = 0
+    l = np.where(l == 0, 0.1, 0.9)
     l_degisik = np.copy(l)
-    l_degisik[4 ,4],l_degisik[3, 4],l_degisik[4, 9],l_degisik[3 ,9] = 1,0,1,0
-    h= np.ones((5, 10),dtype=float)
+    l_degisik[4 ,4],l_degisik[3, 4],l_degisik[4, 9],l_degisik[3 ,9] = 0.9,0.1,0.9,0.1
+    h= np.ones((5, 10))
     h[:, 2] = 0
     h[:, 6] = 0
     h[2, :] = 0
+    h = np.where(h == 0, 0.1, 0.9)
     h_degisik = np.copy(h)
-    h_degisik[0,8],h_degisik[2,6],h_degisik[0,0],h_degisik[2,2] = 0,1,0,1
-    a= np.ones((5, 10),dtype=float)
+    h_degisik[0,8],h_degisik[2,6],h_degisik[0,0],h_degisik[2,2] = 0.1,0.9,0.1,0.9
+    a= np.ones((5, 10))
     a[:, 9] = 0
     a[0, :] = 0
+    a = np.where(a == 0, 0.1, 0.9)
     a_degisik = np.copy(a)
-    a_degisik[0, 2],a_degisik[1, 2],a_degisik[3, 8],a_degisik[3, 9] = 1,0,0,1
+    a_degisik[0, 2],a_degisik[1, 2],a_degisik[3, 8],a_degisik[3, 9] = 0.9,0.1,0.1,0.9
     return t, h, a, l, t_degisik, h_degisik, a_degisik, l_degisik 
 
 #harfe gürültü ekleme
-def gray_noise(harf):
-    new_signal = harf + np.random.normal(0, .01, harf.shape)
-    return new_signal
+def gray_noise(t, h, a, l):
+    t1 = t + np.random.normal(0, .05, t.shape)
+    h1 = h + np.random.normal(0, .05, h.shape)
+    a1 = a + np.random.normal(0, .05, a.shape)
+    l1 = l + np.random.normal(0, .05, l.shape)
+    return t1, h1, a1, l1
 
+#Veri kümelerini eğitime sokmaan önce vektör haline getirme
+def data_vectorizer(data_to_vector):
+    data_to_vector = [a.reshape((50,1)) for a in data_to_vector]
+    return data_to_vector
+
+#veri oluşturan ve gürültü ekleyen fonksiyonlar çağrıldı.
 t, h, a, l, t_degisik, h_degisik, a_degisik, l_degisik  = pattern_designer()
-""" print(t, h, a, l)
- """
-print(type(t))
-""" image=gray_noise(image) """
+t_noisy, h_noisy, a_noisy, l_noisy = gray_noise(t, h, a, l)
 
-""" noisy= image + 0.2* np.random.rand(5,10) """
-""" print(image[0]) """
-for i in [t,h,l,a,t_degisik, h_degisik, a_degisik, l_degisik ]:
+#her bir patter birbirine dik ve eşit uzaklıkta vektörler belirlendi
+e1=[1,0,0,0]
+e2=[0,1,0,0]
+e3=[0,0,1,0]
+e4=[0,0,0,1]
+
+x_input = [t, h, a, l, t_degisik, h_degisik, a_degisik, l_degisik, t_noisy, h_noisy, a_noisy, l_noisy]
+for i in x_input:
     plt.imshow(i, cmap='gray')
     plt.show()
 
-#0 ve 1 in 0.1 ve 0.9 a çekilmesi için kullanılacak
-print(a)
-# [[0 1 2]
-#  [3 4 5]
-#  [6 7 8]]
+#eğitim kümesindeki veriler sınıflandırıldı ve vektörleştirildi
+y_desired = [e1, e2, e3, e4, e1, e2, e3, e4, e1, e2, e3, e4]
+x_input = data_vectorizer(x_input)
 
-print(np.where(a < 4, -1, 100))
-# [[ -1  -1  -1]
-#  [ -1 100 100]
-#  [100 100 100]] """
-#önce total veri kümesini yd ler ile beraber birleştir. 
-#farklı veiler ile test kümesi oluşturulması
-#Tüm verileri vektöre çevir
+
+#test kümesi oluşturuldu ve vektörleştirildi
+t_noisy_test, h_noisy_test, a_noisy_test, l_noisy_test = gray_noise(t, h, a, l)
+x_test = data_vectorizer([t, h, a, l, t_noisy_test, h_noisy_test, a_noisy_test, l_noisy_test])
+y_desired_test = [e1, e2, e3, e4, e1, e2, e3, e4]
+
+#Veriler tekrar kullanılmak için kaydedildi
+np.save("x_egitim.csv", x_input)
+np.save("yd_egitim.csv", y_desired)
+np.save("x_test.csv", x_test)
+np.save("yd_test.csv", y_desired_test)
+
+for i in x_input:
+    plt.imshow(i, cmap='gray')
+    plt.show()
+
+
+#[x]önce total veri kümesini yd ler ile beraber birleştir. 
+#[x]farklı veiler ile test kümesi oluşturulması
+#[x]Tüm verileri vektöre çevir, kaydet, normalize et
 #gradyan hesaplayan fonkfiyon, aktivasyon fonksiyon, momentum terimi unutma
 #ileri yol ve geri yol fonkfiyon
 #ağırlık göncelleme
--- trying.py
+++ trying.py
@@ -2,16 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-x= np.zeros((50,50))
-""" y=np.reshape(x, (50,1)) """
+t= np.ones((5, 10))
+t [:, 5] = 0
+t[0, :] = 0
+t = np.where(t == 0, 0.1, 0.9)
+t_degisik = np.copy(t)
+t_degisik[0 ,0],t_degisik[1 ,0],t_degisik[3 ,4],t_degisik[3 ,5] = 1,0,0,1
+print(t_degisik)
+for i in [t,t_degisik]:
+    plt.imshow(i, cmap='gray')
+    plt.show()
 
-""" print(np.random.rand(50, 50)) """
-noisy = x + 0.1 * np.random.rand(50, 50)
+""" print(t)
+x_tryi = [t,l]
+x_tryi = [a.reshape((50,1)) for a in x_tryi] """
 
-noisy = noisy/noisy.max()
-
-plt.imshow(noisy, cmap='gray')
-plt.show()
-
-cv2.imshow('s',noisy)
-cv2.waitKey(0)
+""" t = np.reshape(np.where(t == 0, 0.1, 0.9), (50, 1)) """
+print(t)
+print(x_tryi)
+print(l)
